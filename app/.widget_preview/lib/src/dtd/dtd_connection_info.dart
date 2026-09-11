f27b15fd1cd77557c3186d7cd407ba69a9b88e0a
hjo331 <hjo331@ssts.co.kr>
2026-09-11 11:04:04 +0900

    폴더 변경 커밋 푸시

---
+++ app/.widget_preview/lib/src/dtd/dtd_connection_info.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: implementation_imports
+
+const String kWidgetPreviewDtdUri = 'ws://127.0.0.1:50977/0GeZFKmJHPc=';
+const String kWidgetPreviewService =
+    'widget-preview-6d2ac977-0024-4840-863e-6aebf114a029';
+const String kWidgetPreviewScaffoldStream =
+    'WidgetPreviewScaffold-6d2ac977-0024-4840-863e-6aebf114a029';
+const String kProjectRootPath = r'C:\PETWALK\app';
